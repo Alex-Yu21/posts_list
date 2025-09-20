f98284d5cbcd85c0c7dd95c6c00842128f8e4e1e
Alex-Yu21 <yualex2112@gmail.com>
2025-09-20 19:42:50 +0200

    feat(main): add ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:posts_list/ui/views/posts_list_view.dart';
 
-void main() => runApp(const MaterialApp(home: PostsListView()));
+void main() {
+  runApp(const ProviderScope(child: App()));
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      home: const PostsListView(),
+    );
+  }
+}

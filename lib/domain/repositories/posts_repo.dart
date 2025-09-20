bee490f1fb9c8056131bc67db77f24f9870fb514
Alex-Yu21 <yualex2112@gmail.com>
2025-09-20 13:44:10 +0200

    feat(domain): add PostsRepo contract (fetchAll/getPost/refresh)

---
+++ lib/domain/repositories/posts_repo.dart
@@ -0,0 +1,9 @@
+import 'package:posts_list/domain/entities/post_entity.dart';
+
+abstract class PostsRepo {
+  Future<List<Post>> fetchAll();
+
+  Future<Post> getPost(int id);
+
+  Future<void> refresh();
+}
